8c490d5b68964933eda222182e24a8a00727155c
Deni Ef <54572928+de956@users.noreply.github.com>
2020-04-06 15:20:33 +0300

    Added .ovpn file configuration script

---
+++ make-vpn.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#OpenVPN file configurations
+ 
+CLIENT_NAME=$1
+OPENVPN_SERVER="your_ip"
+CA_DIR=/etc/openvpn/openvpn-ca
+CLIENT_DIR=/etc/openvpn/clients
+ 
+cd ${CA_DIR}
+source vars
+./build-key ${CLIENT_NAME}
+ 
+echo "client
+dev tun
+proto udp
+remote ${OPENVPN_SERVER} 1194
+user nobody
+group nogroup
+persist-key
+persist-tun
+cipher AES-128-CBC
+auth SHA256
+key-direction 1
+remote-cert-tls server
+comp-lzo
+verb 3" > ${CLIENT_DIR}/${CLIENT_NAME}.ovpn
+ 
+cat <(echo -e '<ca>') \
+    ${CA_DIR}/keys/ca.crt \
+    <(echo -e '</ca>\n<cert>') \
+    ${CA_DIR}/keys/${CLIENT_NAME}.crt \
+    <(echo -e '</cert>\n<key>') \
+    ${CA_DIR}/keys/${CLIENT_NAME}.key \
+    <(echo -e '</key>\n<tls-auth>') \
+    ${CA_DIR}/keys/ta.key \
+    <(echo -e '</tls-auth>') \
+    >> ${CLIENT_DIR}/${CLIENT_NAME}.ovpn
+ 
+echo -e "Client File Created - ${CLIENT_DIR}/${CLIENT_NAME}.ovpn"
